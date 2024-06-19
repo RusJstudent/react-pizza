fcae67a69dce4cc9ddcb60d0ccbd8197cc3d46de
Sergey RJS <sergeydes@yandex.ru>
2024-06-19 11:10:50 +0300

    Unit 13, 0:03:00

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+
+  },
+})
+
+export const {  } = filterSlice.actions
+
+export default filterSlice.reducer
+
+
---
+++ src/redux/slices/sortSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+
+};
+
+export const sortSlice = createSlice({
+    name: 'sort',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export const { } = sortSlice.actions;
+export default sortSlice.reducer;
